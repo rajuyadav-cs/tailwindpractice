43096e1e21d9084569903c8f7383ee525e5df17d
rajuyadav-cs <rajuyadav242625@gmail.com>
2026-07-06 18:36:30 +0530

    tailwindpractice start

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <main>
+        <div className="flex flex-col bg-blue-300 text-blue-300">Home</div>
+        <div className="flex flex-col bg-red-300 text-black-300">About</div>
+        <div className="flex flex-col bg-green-300 text-yellow-700">Links</div>
+        <div className="flex flex-col bg-purple-300 text-gray-300 ">
+          Section
+        </div>
+      </main>
+    </>
+  );
+}
